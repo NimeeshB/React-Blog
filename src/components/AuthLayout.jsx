2fc985082d1a182d08902ed355a6c482cf83a333
Nimeesh <bagwe.nimeesh4@gmail.com>
2025-04-09 18:13:47 -0400

    reverting commit bb2c2da

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -6,7 +6,7 @@ export default function Protected({children, authentication = true}) {
     const navigate = useNavigate()
     const [loader, setLoader] = useState(true)
     const authStatus = useSelector(state => state.auth.status)
-    console.log(authStatus + " :Value from Authlayout, authStatus")
+    console.log(authStatus)
 
     useEffect(() => {
         //TODO: make it more easy to understand
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,27 +7,17 @@ import { useState, useEffect } from 'react'
 
 function Home() {
   const [posts, setPosts] = useState([])
-  const [loading, setLoading] = useState(true);
   
 
   useEffect(() => {
       service.getPosts([]).then((posts) => {
         console.log(posts)
           if (posts) {
-              setPosts(posts.documents);
+              setPosts(posts.documents)
           }
       })
-      .catch((error) => {
-        console.error('Failed to fetch posts:', error);
-    }).finally(() => {
-        setLoading(false);
-    });
   }, [])
 
-  if (loading) 
-    {
-        return <div className="text-center mt-10">Loading posts...</div>;
-    }
   if (posts.length === 0) {
       return (
           <div className="w-full py-8 mt-4 text-center">
